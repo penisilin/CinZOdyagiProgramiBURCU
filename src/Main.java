9bfd01c9e8653a9226ddd287571c26743b2c579d
penisilin <112887815+penisilin@users.noreply.github.com>
2022-11-18 00:06:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input =new Scanner(System.in);
+
+        double dogum;
+        double bolen;
+        double kalan;
+
+        System.out.print("Dogum Yılını Gİriniz :");
+        dogum=input.nextInt();
+
+
+        bolen= dogum/12;
+        kalan= dogum%12;
+
+        if (kalan==0)
+            System.out.print("ÇİN ZODYAĞI BURCUNUZ MAYMUN ");
+        if (kalan==1)
+            System.out.print("ÇİN ZODYAĞI BURCUNUZ HOROZ ");
+        if (kalan==2)
+            System.out.print("ÇİN ZODYAĞI BURCUNUZ KÖPEK ");
+        if (kalan==3)
+            System.out.print("ÇİN ZODYAĞI BURCUNUZ DOMUZ ");
+        if (kalan==4)
+            System.out.print("ÇİN ZODYAĞI BURCUNUZ FARE ");
+        if (kalan==5)
+            System.out.print("ÇİN ZODYAĞI BURCUNUZ ÖKÜZ ");
+        if (kalan==6)
+            System.out.print("ÇİN ZODYAĞI BURCUNUZ KAPLAN ");
+        if (kalan==7)
+            System.out.print("ÇİN ZODYAĞI BURCUNUZ TAVŞAN ");
+        if (kalan==8)
+            System.out.print("ÇİN ZODYAĞI BURCUNUZ EJDARHA ");
+        if (kalan==9)
+            System.out.print("ÇİN ZODYAĞI BURCUNUZ YILAN ");
+        if (kalan==10)
+            System.out.print("ÇİN ZODYAĞI BURCUNUZ AT ");
+        if (kalan==11)
+            System.out.print("ÇİN ZODYAĞI BURCUNUZ KOYUN ");
+
+
+
+
+
+    }
+
+        }
+
+
+
